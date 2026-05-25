13bef87dcdfc08a1746cfa8f0f3053d5a2f003ce
Ramzi-Hadrouk <hadrouk_ramzi_@hotmail.com>
2026-05-25 13:49:35 +0100

    update java version to 26 & add dependencies

--- src/main/java/io/epsilon/auth_spring/Application.java
+++ src/main/java/io/epsilon/auth_spring/Application.java
@@ -2,13 +2,11 @@ package io.epsilon.auth_spring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration; // Add this line
+import org.springframework.boot.jdbc.autoconfigure.DataSourceAutoConfiguration;  // <-- new import
 
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class Application {
-
 	static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
-
 }
