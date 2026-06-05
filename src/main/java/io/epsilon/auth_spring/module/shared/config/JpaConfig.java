5bce266459302eea5f288d2322346ec3ec828e39
Ramzi-Hadrouk <hadrouk_ramzi_@hotmail.com>
2026-06-05 23:42:07 +0100

    Enable Jpa Auditing

---
+++ src/main/java/io/epsilon/auth_spring/module/shared/config/JpaConfig.java
@@ -0,0 +1,8 @@
+package io.epsilon.auth_spring.module.shared.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@Configuration
+@EnableJpaAuditing
+public class JpaConfig {}
