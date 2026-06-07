ee5d5b5c75496dd3fb7cc53f8738901a2bffcbb6
Ramzi-Hadrouk <hadrouk_ramzi_@hotmail.com>
2026-06-07 22:49:06 +0100

    Enable Scheduling

---
+++ src/main/java/io/epsilon/auth_spring/module/shared/config/SchedulingConfig.java
@@ -0,0 +1,7 @@
+package io.epsilon.auth_spring.module.shared.config;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@Configuration
+@EnableScheduling
+public class SchedulingConfig {}
